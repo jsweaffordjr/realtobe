0ff301c81a12ce9423357a19b0aec1151c0c560e
Jerry Sweafford Jr <jsweaffordjr@gmail.com>
2022-01-31 20:03:12 -0600

    added plot scrolling, P control of capture point estimate (ankles)

--- catkin_ws/src/tobe_real/scripts/realtime_plot.py
+++ catkin_ws/src/tobe_real/scripts/realtime_plot.py
@@ -11,26 +11,32 @@ from std_msgs.msg import Float64
 # this script subscribes to the /push and /lean topics, generates real-time plot(s)
 class Visualizer:
     def __init__(self):
+        # pause until after IMU calibration is completed:
         self.calib=False
         rospy.sleep(5)
         self.sub_cal=rospy.Subscriber('/calibration',Vector3,self._calibrate, queue_size=1)
         while self.calib == False:
             rospy.sleep(0.1)
         rospy.sleep(10) 
+        
+        # plot initialization:
         self.fig, (self.ax1, self.ax2, self.ax3) = plt.subplots(3, sharex=True)
         self.fig.set_size_inches(10, 10)
-        self.a1, = self.ax1.plot([], [], label='Lean rate', color='red')
-        self.a2, = self.ax1.plot([], [], label='Lean', color='black')
-        self.a3, = self.ax2.plot([], [], 'b')
-        self.a4, = self.ax3.plot([], [], label='Right ankle', color='black')
-        self.a5, = self.ax3.plot([], [], label='Left hip', color='green')
-        self.a6, = self.ax3.plot([], [], label='Right shoulder', color='red')
-        self.a7, = self.ax3.plot([], [], label='Left shoulder', color='blue')
-        self.a8, = self.ax3.plot([], [], label='Right hip', color='red')
+        self.a1, = self.ax1.plot([], [], label='Lean rate', color='black')
+        self.a2, = self.ax1.plot([], [], label='Lean angle', color='red')
+        self.a3, = self.ax2.plot([], [], label='Z-direction', color='blue')
+        self.a4, = self.ax3.plot([], [], label='Right ankle', color='blue')
+        #self.a5, = self.ax3.plot([], [], label='Left hip', color='green')
+        #self.a6, = self.ax3.plot([], [], label='Right shoulder', color='red')
+        #self.a7, = self.ax3.plot([], [], label='Left shoulder', color='black')
+        #self.a8, = self.ax3.plot([], [], label='Right hip', color='red')
         self.a9, = self.ax3.plot([], [], label='Left ankle', color='green')
-        self.t1, self.t2, self.t3, self.t4, self.t5, self.t6, self.t7, self.t8, self.t9 = [], [], [], [], [], [], [], [], []
-        self.z_lean, self.discrete, self.acc, self.r_ankle, self.l_hip, self.r_shoulder, self.l_shoulder, self.r_hip, self.l_ankle = [], [], [], [], [], [], [], [], []
+        self.t1, self.t2, self.t4, self.t9 = [], [], [], []
+        self.lean, self.lean_dot, self.acc, self.r_ankle, self.l_ankle = [], [], [], [], []
         self.initial_time = rospy.get_time()
+        self.plot_length = 10 # duration of the plot window
+        self.t_start=0
+        self.t_end=self.plot_length
         self.fig.suptitle('Real-time plots')
         
     def _calibrate(self,msg):
@@ -41,75 +47,107 @@ class Visualizer:
             self.calib=True
             
     def init_plot(self):
-        self.ax1.set_title('Longitudinal lean and lean rate')
-        self.ax1.set_xlim(0,90)
-        self.ax1.set_ylim(-2,2)
-        self.ax1.legend(loc='center right')
+        self.ax1.set_title('Longitudinal lean angle and rate')
+        self.ax1.set_xlim(0,self.plot_length)
+        self.ax1.set_ylim(-0.8,0.8)
+        self.ax1.legend(loc='center left')
         self.ax2.set_title('Torso forward acceleration (m/s^2)')
-        self.ax2.set_xlim(0,90)
-        self.ax2.set_ylim(-6,6)
-        self.ax3.set_title('Joint commands (radians)')
-        self.ax3.set_xlim(0,90)
-        self.ax3.set_ylim(-1.5,1.5)
-        self.ax3.legend(loc='center right')
-        return self.ax1, self.ax2, self.ax3
-        
+        self.ax2.set_xlim(0,self.plot_length)
+        self.ax2.set_ylim(-3,3)
+        self.ax2.legend(loc='center left')
+        self.ax3.set_title('Commanded joint angles')
+        self.ax3.set_xlim(0,self.plot_length)
+        self.ax3.set_ylim(-2,2)
+        self.ax3.legend(loc='center left')
+        return self.ax1, self.ax2, self.ax3        
+
+    def update_plot_length(self):
+        self.ax1.set_xlim(self.t_start,self.t_end)
+        self.ax2.set_xlim(self.t_start,self.t_end)
+        self.ax3.set_xlim(self.t_start,self.t_end)
+        return self.ax1, self.ax2, self.ax3  
+                
     def plot_update(self, frame):
-        self.a2.set_data(self.t1,self.z_lean)
-        self.a1.set_data(self.t2,self.discrete)
-        self.a3.set_data(self.t3,self.acc)
+        self.a2.set_data(self.t1,self.lean)
+        self.a1.set_data(self.t1,self.lean_dot)
+        self.a3.set_data(self.t2,self.acc)
         self.a4.set_data(self.t4,self.r_ankle)
-        self.a5.set_data(self.t5,self.l_hip)
-        self.a6.set_data(self.t6,self.r_shoulder)
-        self.a7.set_data(self.t7,self.l_shoulder)
-        self.a8.set_data(self.t8,self.r_hip)
+        #self.a5.set_data(self.t5,self.l_hip)
+        #self.a6.set_data(self.t6,self.r_shoulder)
+        #self.a7.set_data(self.t7,self.l_shoulder)
+        #self.a8.set_data(self.t8,self.r_hip)
         self.a9.set_data(self.t9,self.l_ankle)
-        return self.a1, self.a2, self.a3, self.a4, self.a5, self.a6, self.a7, self.a8, self.a9
-        
-    def lean_callback(self, msg):
-        tnow=rospy.get_time()-self.initial_time
-        self.t1.append(tnow)
-        self.z_lean.append(msg.z)
+        self.update_plot_length()
+        return self.a1, self.a2, self.a3, self.a4, self.a9 #, self.a5, self.a6, self.a7, self.a8
 
     def ldata_callback(self, msg):
-        tnow=rospy.get_time()-self.initial_time
-        self.t2.append(tnow)
-        self.discrete.append(msg.data)
+        tnow=rospy.get_time()-self.initial_time       
+        self.t1.append(tnow)
+        self.lean.append(msg.x)
+        self.lean_dot.append(msg.y)
+        
+        # for scrolling plot: (NOTE: if scrolling plot is not desired, comment out the remainder of this callback)
+        # after the length of plot window has passed, remove the oldest data point each time a new one comes in
+        if tnow > self.plot_length: 
+            self.t1.pop(0)
+            self.lean.pop(0)
+            self.lean_dot.pop(0)
+            self.t_end=tnow
+            self.t_start=tnow-self.plot_length
 
     def pdata_callback(self, msg):
         tnow=rospy.get_time()-self.initial_time
-        self.t3.append(tnow)
+        self.t2.append(tnow)
         self.acc.append(msg.data)
+        
+        # for scrolling plot: (NOTE: if scrolling plot is not desired, comment out the remainder of this callback)
+        # after the length of plot window has passed, remove the oldest data point each time a new one comes in
+        if tnow > self.plot_length: 
+            self.t2.pop(0)
+            self.acc.pop(0)
             
     def r_ankle_callback(self, msg):
         tnow=rospy.get_time()-self.initial_time
         self.t4.append(tnow)
         self.r_ankle.append(msg.data)
         
-    def l_hip_callback(self, msg):
-        tnow=rospy.get_time()-self.initial_time
-        self.t5.append(tnow)
-        self.l_hip.append(msg.data)  
+        # for scrolling plot: (NOTE: if scrolling plot is not desired, comment out the remainder of this callback)
+        # after the length of plot window has passed, remove the oldest data point each time a new one comes in
+        if tnow > self.plot_length: 
+            self.t4.pop(0)
+            self.r_ankle.pop(0)
+        
+    #def l_hip_callback(self, msg):
+    #    tnow=rospy.get_time()-self.initial_time
+    #    self.t5.append(tnow)
+    #    self.l_hip.append(msg.data)  
 
-    def r_shoulder_callback(self, msg):
-        tnow=rospy.get_time()-self.initial_time
-        self.t6.append(tnow)
-        self.r_shoulder.append(msg.data) 
+    #def r_shoulder_callback(self, msg):
+    #    tnow=rospy.get_time()-self.initial_time
+    #    self.t6.append(tnow)
+    #    self.r_shoulder.append(msg.data) 
                
-    def l_shoulder_callback(self, msg):
-        tnow=rospy.get_time()-self.initial_time
-        self.t7.append(tnow)
-        self.l_shoulder.append(msg.data)
+    #def l_shoulder_callback(self, msg):
+    #    tnow=rospy.get_time()-self.initial_time
+    #    self.t7.append(tnow)
+    #    self.l_shoulder.append(msg.data)
                         
-    def r_hip_callback(self, msg):
-        tnow=rospy.get_time()-self.initial_time
-        self.t8.append(tnow)
-        self.r_hip.append(msg.data)        
+    #def r_hip_callback(self, msg):
+    #    tnow=rospy.get_time()-self.initial_time
+    #    self.t8.append(tnow)
+    #    self.r_hip.append(msg.data)        
                
     def l_ankle_callback(self, msg):
         tnow=rospy.get_time()-self.initial_time
         self.t9.append(tnow)
         self.l_ankle.append(msg.data)
+        
+        # for scrolling plot: (NOTE: if scrolling plot is not desired, comment out the remainder of this callback)
+        # after the length of plot window has passed, remove the oldest data point each time a new one comes in
+        if tnow > self.plot_length:
+            self.t9.pop(0)
+            self.l_ankle.pop(0)
+
 
                                        
 if __name__ == "__main__":
@@ -118,15 +156,14 @@ if __name__ == "__main__":
 
     rospy.loginfo("Real-time data plot begins after calibration")
     viz = Visualizer()
-    sub1 = rospy.Subscriber('/lean', Vector3, viz.lean_callback)
-    sub2 = rospy.Subscriber('/leandata', Float64, viz.ldata_callback)
+    sub2 = rospy.Subscriber('/leandata', Vector3, viz.ldata_callback)
     sub3 = rospy.Subscriber('/pushdata', Float64, viz.pdata_callback)
     sub4 = rospy.Subscriber('/tobe/l_ankle_swing_joint_position_controller/command', Float64, viz.l_ankle_callback)
     sub5 = rospy.Subscriber('/tobe/r_ankle_swing_joint_position_controller/command', Float64, viz.r_ankle_callback) 
-    sub6 = rospy.Subscriber('/tobe/l_hip_swing_joint_position_controller/command', Float64, viz.l_hip_callback) 
-    sub7 = rospy.Subscriber('/tobe/r_hip_swing_joint_position_controller/command', Float64, viz.r_hip_callback) 
-    sub8 = rospy.Subscriber('/tobe/l_shoulder_swing_joint_position_controller/command', Float64, viz.l_shoulder_callback)
-    sub9 = rospy.Subscriber('/tobe/r_shoulder_swing_joint_position_controller/command', Float64, viz.r_shoulder_callback)
+    #sub6 = rospy.Subscriber('/tobe/l_hip_swing_joint_position_controller/command', Float64, viz.l_hip_callback) 
+    #sub7 = rospy.Subscriber('/tobe/r_hip_swing_joint_position_controller/command', Float64, viz.r_hip_callback) 
+    #sub8 = rospy.Subscriber('/tobe/l_shoulder_swing_joint_position_controller/command', Float64, viz.l_shoulder_callback)
+    #sub9 = rospy.Subscriber('/tobe/r_shoulder_swing_joint_position_controller/command', Float64, viz.r_shoulder_callback)
     ani = FuncAnimation(viz.fig, viz.plot_update, init_func=viz.init_plot)
     plt.show(block=True)
     
--- catkin_ws/src/tobe_real/src/tobe_real/stand.py
+++ catkin_ws/src/tobe_real/src/tobe_real/stand.py
@@ -4,6 +4,9 @@ from threading import Thread
 import rospy
 import math
 import numpy as np
+import random
+from numpy import array
+from numpy.linalg import norm
 from tobe_real.tobe import Tobe
 from geometry_msgs.msg import Vector3
 from std_msgs.msg import Float64
@@ -17,11 +20,13 @@ class StandFunc:
 
     def __init__(self):
         # set initial joint angles
-        angles = {}
-
+        angles = {} # empty array for angle commands
+        
+        # array of initial joint angle commands
         f = [-0.2, -1.1, -1.7264, 0.5064, -0.15, 0, 0, 0.15, -0.5064, 1.7264,
              1.1, 0.2, -0.3927, -0.3491, -0.5236, 0.3927, -0.3491, 0.5236]
 
+        # assign command value to each joint
         angles["l_ankle_lateral_joint"] = f[0]
         angles["l_ankle_swing_joint"] = f[1]
         angles["l_knee_joint"] = f[2]
@@ -40,39 +45,26 @@ class StandFunc:
         angles["r_shoulder_swing_joint"] = f[15]
         angles["r_shoulder_lateral_joint"] = f[16]
         angles["r_elbow_joint"] = f[17]     
-        self.init_angles = angles  
+        
+        self.init_angles = angles  # set initial array to assigned 'angles'
         self.qz_min = 0.02 # lean threshold: values less than this (~1.15 deg.) are ignored
         self.az_min = 0.1 # push threshold: values less than this are ignored
         
-    def get_ankles_hips(self, z_lean, l_deriv, z_push, gain_to_be_tuned):
+    def get_angles(self, z_lean, l_deriv, l_ddot, gain1):
         # controller gains
-        Kpa = 0.01 # proportional gain for sag. ankles       
-        Kih = 0.1 
+        Kpa = gain1 # proportional gain for ankles    
+        Kda = 0 # derivative gain for ankles
         
-        Kps = 0.2 # proportional gain for sag. shoulders
-        Kis = 0.5 # integral gain for sag. shoulders
-        
-        # thresholds for ankle, hip, shoulder control:
-        minqz = 0.02
-        maxqz = 0.1
-        maxlrate = gain_to_be_tuned
-        
-        # compute ankle, hip adjustments:
+        # add 'diff' and 'diff2' to initial ankle, hip joint commands, respectively:
         diff = 0
         diff2 = 0
-        diff3 = 0
         
-        if (abs(z_lean[4]) > minqz):
-            """ PID control of sagittal ankles""" 
-            diff = Kpa*z_lean[4] # P control of ankles
-            if (abs(z_lean[4]) > maxqz):
-                diff2 = Kih*z_lean[4] # I control of hips
-                diff3 = Kis*z_lean[4] # I control
-                
-        if (abs(l_deriv) > maxlrate):    
-            diff3 = diff3 + Kps*l_deriv  # PI control of shoulders          
-        
-        """ Set joint angles"""
+        # compute ankle adjustments:
+        h = 0.29 # height of IMU from ground
+        g = 9.81 # acceleration due to gravity
+        omega_inv = math.sqrt(h/g)
+        diff = Kpa*(z_lean[4]+omega_inv*l_deriv) + Kda*(l_deriv+omega_inv*l_ddot)  # PD control of ankles (capture point) 
+
         # set initial joint angles
         angles = self.init_angles
 
@@ -90,8 +82,8 @@ class StandFunc:
         angles["r_ankle_swing_joint"] = f2 - diff
         angles["l_hip_swing_joint"] = f[3] + diff2
         angles["r_hip_swing_joint"] = f[8] - diff2
-        angles["l_shoulder_swing_joint"] = f[12] + diff3
-        angles["r_shoulder_swing_joint"] = f[15] - diff3
+        #angles["l_shoulder_swing_joint"] = f[12] + diff3
+        #angles["r_shoulder_swing_joint"] = f[15] - diff3
 
         return angles
 
@@ -101,76 +93,139 @@ class Stand:
     """
 
     def __init__(self, tobe):
+        self.func = StandFunc()
+        
+        # pause until IMU is calibrated:
+        self.calib=False
+        rospy.sleep(5)
+        self.sub_cal=rospy.Subscriber('/calibration',Vector3,self._calibrate, queue_size=1)
+        #rospy.loginfo("Vc: ")
+        #rospy.loginfo(self.func.Vc)
+        while self.calib == False:
+            rospy.sleep(0.1)
+        rospy.sleep(10) 
+    
+        # initialization parameters:
         self.tobe = tobe
         self.active = False
         self.standing = False
-        self.func = StandFunc()
         self.ready_pos = self.func.init_angles
-
         self._th_stand = None
         self.response = self.ready_pos
 
+        # other variables, parameters:
         self.push=0 # smoothed z-acceleration value
         self.lean=[0,0,0,0,0] # last 5 values from 'lean' publisher
         self.l_deriv=0
-        self.z_next=[0,0,0]
-        self.kgain=0.001
+        self.l_ddot=0
+        self.l_int=0
+        self.ldata=Vector3()
+        self.z_next=[0,0,0,0]
+        self.var1=0.008
+        self.var2=0.0
         self.qz_min = self.func.qz_min # lean threshold: values less than this are rounded down to zero
         self.az_min = self.func.az_min # push threshold: values less than this are rounded down to zero
+        self.time=0
         
+        # subscribers and publishers:
         self._sub_quat = rospy.Subscriber("/lean", Vector3, self._update_orientation, queue_size=5) # subscribe to lean topic
         self._sub_acc = rospy.Subscriber("/push", Vector3, self._update_acceleration, queue_size=5) # subscribe to push topic
-        self.leandata = rospy.Publisher('leandata', Float64, queue_size=1)
+        self.leandata = rospy.Publisher('leandata', Vector3, queue_size=1)
         self.pushdata = rospy.Publisher('pushdata', Float64, queue_size=1)
-        self.Ktuning = rospy.Publisher('kgain', Float64, queue_size=1)
-        self._sub_gain = rospy.Subscriber("/kgain", Float64, self._update_gain, queue_size=5)
-
+        self.Ktuning = rospy.Publisher('var1', Float64, queue_size=1)
+        self.Ktuning2 = rospy.Publisher('var2', Float64, queue_size=1)
+        self._sub_gain = rospy.Subscriber("/var1", Float64, self._update_gain1, queue_size=5)
+        self._sub_gain2 = rospy.Subscriber("/var2", Float64, self._update_gain2, queue_size=5)
+        
+        # switch to 'active' status, move to initial 'home' position, if not already there:
         self.start()
 
-    def _update_gain(self, msg):
-        # update gain for PID control tuning
-        self.kgain = msg.data
+    def _update_gain1(self, msg):
+        # updates 'var1' during experiment
+        self.var1 = msg.data
         
-    
+    def _update_gain2(self, msg):
+        # updates 'var2' during experiment
+        self.var2 = msg.data
+           
     def _update_acceleration(self, msg):
         """
-        Catches acceleration data and applies exponentially weighted moving average to smooth out data
+        Catches acceleration data and applies exponentially weighted moving average to smooth out data output
         """
-        w = 0.6
-        az = msg.z      
-        if abs(az) < self.az_min:
+        w = 0.6 # weight of current data point's contribution to moving average output
+        az = msg.z # get acceleration in z-direction      
+        if abs(az) < self.az_min: # apply threshold
             az = 0    
-        acc=w*az+(1-w)*self.push
-        self.pushdata.publish(acc)
-        self.push=acc
+        acc=w*az+(1-w)*self.push # update exponentially weighted moving average
+        self.pushdata.publish(acc) # publish current (smoothed) acceleration value
+        self.push=acc # save current value in 'push'
                     
     def _update_orientation(self, msg):
         """
-        Catches lean detection data and updates robot orientation
+        Catches lean angle data and updates robot orientation
         """
-        qz = msg.z
-        if abs(qz) < self.qz_min:
-            qz = 0          
-        self.lean.pop(0)
-        self.lean.append(qz) # update the newest orientation data
+        q = msg.z # get z-(i.e., forward/backward direction)component of initially vertical x-axis
+        if abs(q) < self.qz_min: # apply threshold
+            q = 0
+            if self.lean[1]+self.lean[2]+self.lean[3]+self.lean[4] == 0:
+                self.l_int = 0 # reset integrator if past five values are zero
         
-        # derivative estimates:
+        #qz=q # use lean factor instead of lean angle
+        qz = math.asin(q) # convert lean factor to lean angle (inverse sine of z-component of IMU x-axis [which is a unit vector])   
+        self.lean.pop(0) # remove oldest value from array of five previous lean angle values
+        self.lean.append(qz) # append newest value to end of the array
+        
+        # derivative and integral estimates:
         dt=0.02 # approximate dt between data points
         
-        # homogeneous discrete differentiator, assuming 50 Hz rate
+        area=0.5*dt*(self.lean[3]+self.lean[4]) # trapezoidal integration between two values
+        prev=self.l_int # get previous value of integral
+        self.l_int=prev+area # updated integral value
+        
+        # homogeneous discrete sliding-mode-based differentiator:
         L=5 # Lipschitz constant
+        
+        # option 1: if only 1 derivative (z0dot) is needed, use this:
+        #z0=self.z_next[0]
+        #z1=self.z_next[1]
+        #z2=self.z_next[2]
+        #z0dot=z1-2.12*(L**(1/3))*(abs(z0-qz)**(2/3))*np.sign(z0-qz)
+        #z1dot=z2-2*(L**(2/3))*(abs(z0-qz)**(1/3))*np.sign(z0-qz)
+        #z2dot=-1.1*L*np.sign(z0-qz)
+        #self.z_next[0]=z0+dt*z0dot+0.5*dt*dt*z2
+        #self.z_next[1]=z1+dt*z1dot
+        #self.z_next[2]=z2+dt*z2dot
+        
+        # option 2: if 2 derivatives (z0dot and z1dot) are needed, use this:
         z0=self.z_next[0]
         z1=self.z_next[1]
         z2=self.z_next[2]
-        z0dot=z1-2.12*(L**(1/3))*(abs(z0-qz)**(2/3))*np.sign(z0-qz)
-        z1dot=z2-2*(L**(2/3))*(abs(z0-qz)**(1/3))*np.sign(z0-qz)
-        z2dot=-1.1*L*np.sign(z0-qz)
-        self.z_next[0]=z0+dt*z0dot+0.5*dt*dt*z1
-        self.z_next[1]=z1+dt*z1dot
+        z3=self.z_next[3]
+        z0dot=z1-3*(L**(1/4))*(abs(z0-qz)**(3/4))*np.sign(z0-qz)
+        z1dot=z2-4.16*(L**(1/2))*(abs(z0-qz)**(1/2))*np.sign(z0-qz)
+        z2dot=z3-3.06*(L**(3/4))*(abs(z0-qz)**(1/4))*np.sign(z0-qz)
+        z3dot=-1.1*L*np.sign(z0-qz)
+        self.z_next[0]=z0+dt*z0dot+0.5*dt*dt*z2+(1/6)*(dt*dt*dt*z3)
+        self.z_next[1]=z1+dt*z1dot+0.5*dt*dt*z3
         self.z_next[2]=z2+dt*z2dot
+        self.z_next[3]=z3+dt*z3dot
 
-        self.l_deriv = z0dot # HDD output
-        self.leandata.publish(self.l_deriv)
+        # HDD output:
+        self.l_deriv = z0dot 
+        self.l_ddot = z1dot # only use this if using option 2
+        
+        # assign lean angle and derivative(s) and/or integral:
+        self.ldata.x=qz
+        self.ldata.y=self.l_deriv
+        self.ldata.z=self.l_ddot
+        self.leandata.publish(self.ldata)
+
+    def _calibrate(self,msg):
+        x=msg.x
+        y=msg.y
+        z=msg.z
+        if x > 2 and y > 2 and z > 2:
+            self.calib=True
 
     def start(self):
         if not self.active:
@@ -197,9 +252,10 @@ class Stand:
         r = rospy.Rate(samplerate)
         rospy.loginfo("Started standing thread")
         func = self.func
-        while not rospy.is_shutdown() and self.standing: # control loop
-            self.response=func.get_ankles_hips(self.lean,self.l_deriv,self.push,self.kgain)
-            self.tobe.set_angles(self.response)      
+        while not rospy.is_shutdown() and self.standing: # control loop     
+            # compute appropriate joint commands and execute commands:           
+            self.response=func.get_angles(self.lean,self.l_deriv,self.l_ddot,self.var1)
+            self.tobe.set_angles(self.response) # set TOBE joint commands to 'response'     
             r.sleep()
         rospy.loginfo("Finished standing control thread")
 
